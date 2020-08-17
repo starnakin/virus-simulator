b6be0f4258d01410d172ac0b6677604f69c255f0
Kuro <yanis.bellot@gmail.com>
2020-08-17 19:51:47 +0200

    l

---
+++ case.py
@@ -0,0 +1,18 @@
+class Case():
+    def __init__(self, has_pop, X, Y):
+        self.has_pop = has_pop
+        self.location = [X, Y]
+
+    def getLocation(self):
+        return self.location
+
+    def hasPop(self):
+        return self.has_pop
+
+    def addPop(self):
+        self.has_pop = True
+    
+    def removePop(self):
+        self.has_pop = False
+
+
