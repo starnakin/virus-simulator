d5cf1f2bc5bbc6a438c647e378af85e2290568b0
starnakin <starnakin2@gmail.com>
2020-08-17 23:01:43 +0200

    IG

---
+++ turn.py
@@ -0,0 +1,10 @@
+class Turn:
+    def __init__(self, case_list, pop_list):
+        self.cases=case_list
+        self.pops=pop_list
+
+    def get_cases(self):
+        return self.cases
+
+    def get_pops(self):
+        return self.pops
